32d3be9ebd93a9c27240d7efd0363555c29c2779
Lyn <james_gosling@163.com>
2020-02-07 15:30:39 +0800

    initial commit

---
+++ server-main/src/com/jhhg/nova/constant/Constant.java
@@ -0,0 +1,19 @@
+package com.jhhg.nova.constant;
+
+/***
+ * @ClassName: Constant
+ * @Description: 用于常量存储
+ * @Author: Lyn
+ * @Date: 2020/2/7 上午10:48
+ * @version : V1.0
+ */
+public interface Constant {
+
+    /**服务器端口号*/
+    int SERVER_PORT = 8008;
+    /**空格*/
+    String BLANK = " ";
+    /**换行符*/
+    String CRLF = "\r\n";
+
+}
